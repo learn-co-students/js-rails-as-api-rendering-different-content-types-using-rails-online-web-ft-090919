ead99d06fdcbde7f6a80d2211c986b307e2264b8
mplichta <bigcatplichta@gmail.com>
2020-01-08 13:26:58 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,14 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    
+    # render as erb (will need instance variable for @birds)
+    # render 'birds/index.html.erb'
+
+    # render as plaintext (this only renders <ActiveRecord_Relation> object)
+    # render plain: birds
+
+    # render JSON
+    render json: birds
   end
 end
